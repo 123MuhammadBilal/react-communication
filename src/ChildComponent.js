e649cb70cda1094df187cd54fd8d3ce5f58aed0a
Muhammad Bilal <kkmuhammadbilal2@gmail.com>
2023-05-02 15:14:53 +0500

    parent and child communication

---
+++ src/ChildComponent.js
@@ -0,0 +1,19 @@
+// ParentComponent.js
+// import React, { useState } from "react";
+// import ChildComponent from "./ChildComponent";
+import React from "react";
+
+export const ChildComponent = () => {
+//   const [message, setMessage] = useState("");
+
+//   const handleMessage = (childMessage) => {
+//     setMessage(childMessage);
+//   };
+
+  return (
+    <>
+      <h2>Parent Component</h2>
+    </>
+  );
+}
+
---
+++ src/ParentComponent.js
@@ -0,0 +1,19 @@
+// ParentComponent.js
+// import React, { useState } from "react";
+// import ChildComponent from "./ChildComponent";
+import React from "react";
+
+export const ParentComponent = () => {
+//   const [message, setMessage] = useState("");
+
+//   const handleMessage = (childMessage) => {
+//     setMessage(childMessage);
+//   };
+
+  return (
+    <>
+      <h2>Parent Component</h2>
+    </>
+  );
+}
+
